af81fb7d830b3b698c1a391b2733fd839afc1896
Pedro Santana <pedromanu27@gmail.com>
2024-07-22 13:00:48 -0300

    feat: :sparkles: Olá mundo em html response

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,24 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert (verificação)
     assert response.json() == {'message': 'Olá, mundo!'}
+
+
+def test_ola_mundo_html_deve_retornar_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/olamundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title>Olá, mundo!</title>
+        </head>
+        <body>
+            <h1>Olá, mundo!</h1>
+        </body>
+    </html>
+    """
+    )
