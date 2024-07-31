b053aa12b74efcf504c035c7e24d97fc568dd3cf
Pedro Santana <pedromanu27@gmail.com>
2024-07-30 21:27:28 -0300

    feat: :sparkles: pydantic

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,17 +3,17 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá, mundo!'}
+    return {"message": "Olá, mundo!"}
 
 
-@app.get('/olamundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+@app.get("/olamundo", status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def ola_mundo_html():
     return """
     <html>
@@ -25,3 +25,8 @@ def ola_mundo_html():
         </body>
     </html>
     """
+
+
+@app.post("/users", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    name: str
+    email: EmailStr
