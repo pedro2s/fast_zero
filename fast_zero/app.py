009a413ee24b208865aa3387172fd18ea2d1f19a
Pedro Santana <pedromanu27@gmail.com>
2024-07-31 00:07:04 -0300

    feat: :sparkles: create, read e test users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,17 +3,19 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message, UserPublic, UserSchema
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Olá, mundo!"}
+    return {'message': 'Olá, mundo!'}
 
 
-@app.get("/olamundo", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+@app.get('/olamundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def ola_mundo_html():
     return """
     <html>
@@ -27,6 +29,14 @@ def ola_mundo_html():
     """
 
 
-@app.post("/users", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    return user
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users', response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (execução)
 
     assert response.status_code == HTTPStatus.OK  # Assert (verificação)
     assert response.json() == {'message': 'Olá, mundo!'}
 
 
-def test_ola_mundo_html_deve_retornar_ola_mundo():
-    client = TestClient(app)
-
+def test_ola_mundo_html_deve_retornar_ola_mundo(client):
     response = client.get('/olamundo')
 
     assert response.status_code == HTTPStatus.OK
@@ -33,3 +25,36 @@ def test_ola_mundo_html_deve_retornar_ola_mundo():
     </html>
     """
     )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'name': 'XXXXX',
+            'email': 'alice@example.com',
+            'password': 'senha123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'name': 'XXXXX',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'name': 'XXXXX',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
